7d5f4685f0116d2717f37c8639674e9083601153
Bartosz Iwiński <01bartosz0001@gmail.com>
2020-02-25 16:03:20 +0100

    Add checking for log in and log out

--- client/app.js
+++ client/app.js
@@ -1,3 +1,6 @@
+const socket = io();
+socket.on('message', ({ author, content }) => addMessage(author, content));
+
 const loginForm = document.querySelector('#welcome-form');
 const messagesSection = document.querySelector('#messages-section');
 const messagesList = document.querySelector('#messages-list');
@@ -6,6 +9,7 @@ const userNameInput = document.querySelector('#username');
 const messageContentInput = document.querySelector('#message-content');
 
 let userName;
+let currentMessage = {};
 
 loginForm.addEventListener('submit', login);
 
@@ -13,20 +17,30 @@ function login(event) {
     event.preventDefault();
     if(userNameInput.value === '') alert('You have to input your name!');
     else {
-        userName = userNameInput.value;
-        messagesSection.classList.add('show');
-        loginForm.classList.remove('show');
+      userName = userNameInput.value;
+      messagesSection.classList.add('show');
+      loginForm.classList.remove('show');
+      socket.emit('logged', userName);
     }
 }
 
 addMessageForm.addEventListener('submit', sendMessage);
 
+function initDate() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const currentDate = hours + ':' + minutes;
+  return currentDate;
+}
+
 function sendMessage(event) {
     event.preventDefault();
     if(messageContentInput.value === '') alert('No message!');
     else {
-        addMessage(userName, messageContentInput.value);
-        messageContentInput.value = '';
+      addMessage(userName, messageContentInput.value);
+      socket.emit('message', { author: userName, content: messageContentInput.value });
+      messageContentInput.value = '';
     }
 }
 
@@ -35,11 +49,23 @@ function addMessage(author, content) {
     message.classList.add('message');
     message.classList.add('message--received');
     if(author === userName) message.classList.add('message--self');
-    message.innerHTML = `
-      <h3 class="message__author">${userName === author ? 'You' : author }</h3>
-      <div class="message__content">
-        ${content}
-      </div>
-    `;
+    if(author === 'Chat Bot') message.classList.add('message--chatbot');
+    if(author === currentMessage.author) {
+      message.innerHTML = `
+        <div class="message__content">
+          ${content}
+        </div>
+      `;
+    }
+    else {
+      message.innerHTML = `
+        <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+        <div class="message__content">
+          ${content}
+        </div>
+      `;
+    }
+    
     messagesList.appendChild(message);
+    currentMessage = {author: author, content: content};
   }
--- server.js
+++ server.js
@@ -1,15 +1,41 @@
 const express = require('express');
 const path = require('path');
+const socket = require('socket.io');
 
 const app = express();
 app.use(express.static(path.join(__dirname, '/client')));
 
 const messages = [];
+const users = [];
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/index.html'));
 });
 
-app.listen(8000, () => {
+const server = app.listen(8000, () => {
   console.log('Server is running on port: 8000');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+  socket.on('message', (message) => { 
+    console.log('Oh, I\'ve got something from ' + socket.id);
+    messages.push({author: message.author, content: message.content}); 
+    socket.broadcast.emit('message', message);
+  });
+  socket.on('logged', (user) => {
+    console.log('User ' + socket.id + ' is logged! Its name is ' + user);
+    users.push({name: user, id: socket.id});
+    socket.broadcast.emit('message', { author: 'Chat Bot', content: `${user} has joined the conversation!` });
+  });
+  socket.on('disconnect', () => { 
+    console.log('Oh, socket ' + socket.id + ' has left');
+    const index = users.findIndex(item => item.id === socket.id);
+    socket.broadcast.emit('message', { author: 'Chat Bot', content: `${users[index].name} has left the conversation!` });
+    users.splice(index, 1);
+  });
+  console.log('I\'ve added a listener on message event \n');
+
 });
